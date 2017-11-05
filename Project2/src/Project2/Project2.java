e8b6de83b6f00ab95927141476f2eff55846a18b
Rimy Ahmed <Rimy Ahmed@LAPTOP-ROVRQQPF>
2017-11-05 14:49:57 -0500

    My 2nd Git Project

---
+++ Project2/src/Project2/Project2.java
@@ -0,0 +1,12 @@
+package Project2;
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		//https://github.com/RimyAhmed/Rimy02.git
+		//System.out.println("Dhaka");
+		//System.out.println("Sylhet");
+
+	}
+
+}
